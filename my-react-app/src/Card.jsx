e347013ca25d3b6fa23f48be10cb16c5ee434e35
raine <raineaveryocampo@gmail.com>
2026-06-22 15:49:57 +0800

    Profile Card

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,26 @@
+import profilepic from "./assets/Komi.jpg";
+
+function Card(props){
+    return(
+        <div className="card">
+            <img src={profilepic} alt="Komi" />
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <p>Age: {props.age}</p>
+        </div>
+    );
+}
+
+Card.propTypes = {
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired
+};
+
+Card.defaultProps = {
+    name: "Unknown",
+    description: "No description available.",
+    age: 0
+};
+
+export default Card;
