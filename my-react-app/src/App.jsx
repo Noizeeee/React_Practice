fcd66c4068707f18fb5d8a395d9847b527fe198b
raine <raineaveryocampo@gmail.com>
2026-06-23 13:49:22 +0800

    Pract Website

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Card from "./Card";
+import Nav from "./Navbar";
 
 function App() {
 
   return (
     <>
+      <Nav/>
       <Card name ="Komi San" description="A talented student council president with a kind heart." age = {18}/>
     </>
   );
---
+++ my-react-app/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar (){
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Services</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
