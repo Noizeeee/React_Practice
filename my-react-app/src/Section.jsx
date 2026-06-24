9e86dd14d67e7ebc8afa69f378539d02dfc0c51b
raine <raineaveryocampo@gmail.com>
2026-06-24 08:39:51 +0800

    Main Section Fix

--- my-react-app/src/Section.jsx
+++ my-react-app/src/Section.jsx
@@ -4,9 +4,9 @@ import Card from "./Card";
 function Section(){
     return(
         <>
-        <section className = "section" id = "home"><Card name="Komi San" description="A talented student council president with a kind heart." age={18} /></section>
+        <section className = "section" id = "home"></section>
         <section className = "section2" id = "about"></section>
-        <section className = "section3" id = "contact"></section>
+        <section className = "section3" id = "contact"><Card name="Komi San" description="A talented student council president with a kind heart." age={18} /></section>
         <section className = "section4" id = "services"></section>
         </>
     );
